b6af0ccb512b684b971a0a110bcdfbf52f800228
NohaSayedA <nohasayedash7@gmail.com>
2019-06-18 16:05:56 +0200

    Change the package of methods

--- core/image.go
+++ core/image.go
@@ -1,12 +1,32 @@
 package pigo
 
 import (
+	"github.com/fogleman/gg"
 	"image"
 	"image/color"
+	"image/jpeg"
 	_ "image/jpeg"
+	"image/png"
 	_ "image/png"
+	"io/ioutil"
+	"math"
 	"os"
+	"path/filepath"
 )
+
+var dc *gg.Context
+
+// faceDetector struct contains Pigo face detector general settings.
+type faceDetector struct {
+	angle        float64
+	cascadeFile  string
+	destination  string
+	minSize      int
+	maxSize      int
+	shiftFactor  float64
+	scaleFactor  float64
+	iouThreshold float64
+}
 
 // GetImage retrieves and decodes the image file to *image.NRGBA type.
 func GetImage(input string) (*image.NRGBA, error) {
@@ -23,6 +43,126 @@ func GetImage(input string) (*image.NRGBA, error) {
 	img := ImgToNRGBA(src)
 
 	return img, nil
+}
+
+// newFaceDetector initialises the constructor function.
+func NewFaceDetector(destination, cf string, minSize, maxSize int, shf, scf, iou, angle float64) *faceDetector {
+	return &faceDetector{
+		angle:        angle,
+		destination:  destination,
+		cascadeFile:  cf,
+		minSize:      minSize,
+		maxSize:      maxSize,
+		shiftFactor:  shf,
+		scaleFactor:  scf,
+		iouThreshold: iou,
+	}
+}
+
+// detectFaces run the detection algorithm over the provided source image.
+func (fd *faceDetector) DetectFaces(source string) ([]Detection, error) {
+	src, err := GetImage(source)
+	if err != nil {
+		return nil, err
+	}
+
+	pixels := RgbToGrayscale(src)
+	cols, rows := src.Bounds().Max.X, src.Bounds().Max.Y
+
+	dc = gg.NewContext(cols, rows)
+	dc.DrawImage(src, 0, 0)
+
+	cParams := CascadeParams{
+		MinSize:     fd.minSize,
+		MaxSize:     fd.maxSize,
+		ShiftFactor: fd.shiftFactor,
+		ScaleFactor: fd.scaleFactor,
+		ImageParams: ImageParams{
+			Pixels: pixels,
+			Rows:   rows,
+			Cols:   cols,
+			Dim:    cols,
+		},
+	}
+
+	cascadeFile, err := ioutil.ReadFile(fd.cascadeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	pigo := NewPigo()
+	// Unpack the binary file. This will return the number of cascade trees,
+	// the tree depth, the threshold and the prediction from tree's leaf nodes.
+	classifier, err := pigo.Unpack(cascadeFile)
+	if err != nil {
+		return nil, err
+	}
+
+	// Run the classifier over the obtained leaf nodes and return the detection results.
+	// The result contains quadruplets representing the row, column, scale and detection score.
+	faces := classifier.RunCascade(cParams, fd.angle)
+
+	// Calculate the intersection over union (IoU) of two clusters.
+	faces = classifier.ClusterDetections(faces, fd.iouThreshold)
+
+	return faces, nil
+}
+
+// drawFaces marks the detected faces with a circle in case isCircle is true, otherwise marks with a rectangle.
+func (fd *faceDetector) DrawFaces(faces []Detection, isCircle bool) ([]byte, []image.Rectangle, error) {
+	var (
+		qThresh float32 = 5.0
+		rects   []image.Rectangle
+	)
+
+	for _, face := range faces {
+		if face.Q > qThresh {
+			if isCircle {
+				dc.DrawArc(
+					float64(face.Col),
+					float64(face.Row),
+					float64(face.Scale/2),
+					0,
+					2*math.Pi,
+				)
+			} else {
+				dc.DrawRectangle(
+					float64(face.Col-face.Scale/2),
+					float64(face.Row-face.Scale/2),
+					float64(face.Scale),
+					float64(face.Scale),
+				)
+			}
+			rects = append(rects, image.Rect(
+				face.Col-face.Scale/2,
+				face.Row-face.Scale/2,
+				face.Scale,
+				face.Scale,
+			))
+			dc.SetLineWidth(2.0)
+			dc.SetStrokeStyle(gg.NewSolidPattern(color.RGBA{R: 255, G: 0, B: 0, A: 255}))
+			dc.Stroke()
+		}
+	}
+
+	img := dc.Image()
+	output, err := os.OpenFile(fd.destination, os.O_CREATE|os.O_RDWR, 0755)
+	defer output.Close()
+
+	if err != nil {
+		return nil, nil, err
+	}
+	ext := filepath.Ext(output.Name())
+
+	switch ext {
+	case ".jpg", ".jpeg":
+		jpeg.Encode(output, img, &jpeg.Options{Quality: 100})
+	case ".png":
+		png.Encode(output, img)
+	}
+	rf, err := ioutil.ReadFile(fd.destination)
+
+	return rf, rects, err
 }
 
 // ImgToNRGBA converts any image type to *image.NRGBA with min-point at (0, 0).
